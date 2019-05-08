39010b88a823e2b8d401b267c24616164f264cb7
Theo Hoekstra <theo.hoekstra@noreply.nl>
2019-05-08 18:00:48 +0200

    Adding / Removing nature functionality in progress

--- src/nl/nnworks/nnembedded/project/nature/NNEmbeddedProjectNature.java
+++ src/nl/nnworks/nnembedded/project/nature/NNEmbeddedProjectNature.java
@@ -4,10 +4,11 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
 
-
 public class NNEmbeddedProjectNature implements IProjectNature {
 
     public static final String NATURE_ID = "nl.nnworks.nnembedded.project.nature";
+    public static final String CNATURE_ID = "org.eclipse.cdt.core.cnature";
+    public static final String CCNATURE_ID = "org.eclipse.cdt.core.ccnature";
 	
     private IProject project;    
     
--- src/nl/nnworks/nnembedded/project/nature/NaturePropertyTester.java
+++ src/nl/nnworks/nnembedded/project/nature/NaturePropertyTester.java
@@ -1,17 +1,59 @@
 package nl.nnworks.nnembedded.project.nature;
 
+import java.util.List;
+
+import org.eclipse.core.commands.operations.OperationStatus;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ui.statushandlers.StatusManager;
+
 public class NaturePropertyTester extends org.eclipse.core.expressions.PropertyTester {
 
-	public NaturePropertyTester() {
-        System.out.println("hola created");			
-	}
+  public static final String CANADDNNEMBEDDEDNATURE_PROERTY = "canAddNNEmbeddedNature";
+  public static final String HASNNEMBEDDEDNATURE_PROPERTY = "hasNNEmbeddedNature";
+  
+  
+  public NaturePropertyTester() {
+  }
 
-	@Override
-	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-	  if ("hasNNEmbeddedNature".equals(property)) {
-        System.out.println("hola");			
-	  }
-	  return true;
-	}
+  @Override
+  public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
 
+    if (receiver instanceof List) {
+      List<?> receiverList = (List<?>) receiver;
+      for (Object obj: receiverList) {
+        try {
+          if (obj instanceof IProject) {
+            IProject project = (IProject) obj; 
+            if (CANADDNNEMBEDDEDNATURE_PROERTY.equals(property)) {
+              if (hasCorCppNature(project) && (project.getNature(NNEmbeddedProjectNature.NATURE_ID) == null)) {
+                return true;
+              }
+            } else if (HASNNEMBEDDEDNATURE_PROPERTY.equals(property)) {
+              if (project.getNature(NNEmbeddedProjectNature.NATURE_ID) != null) {
+                return true;
+              }
+            }
+          }
+        } catch (CoreException e) {
+          // TODO: fix this
+          OperationStatus status = new OperationStatus(1, "blabla", 3, "something went wrong", null);
+          StatusManager.getManager().handle(status, StatusManager.LOG);
+        }        
+      }
+    }
+    
+    return false;
+  }
+  
+  private boolean hasCorCppNature(IProject project) throws CoreException {
+
+    if (project.getNature(NNEmbeddedProjectNature.CNATURE_ID) != null ||
+        project.getNature(NNEmbeddedProjectNature.CCNATURE_ID) != null) {
+      return true;
+    }
+    
+    return false;
+  }
+  
 }
