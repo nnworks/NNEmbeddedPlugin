2d3360b3c6bc2ad3e000fba81472cf3348665717
Theo Hoekstra <theo.hoekstra@noreply.nl>
2019-05-14 18:07:01 +0200

    work in progress

--- src/nl/nnworks/nnembedded/plugin/config/ui/ProjectPropertyPage.java
+++ src/nl/nnworks/nnembedded/plugin/config/ui/ProjectPropertyPage.java
@@ -1,21 +1,41 @@
 package nl.nnworks.nnembedded.plugin.config.ui;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.dialogs.PropertyPage;
 
-public class ProjectPropertyPage extends org.eclipse.ui.dialogs.PropertyPage implements IWorkbenchPropertyPage {
+public class ProjectPropertyPage extends PropertyPage implements IWorkbenchPropertyPage {
 
   public static final String PROPERTYPAGE_ID = "nl.nnworks.nnembedded.plugin.projectpropertiespage";
-  
+
   public ProjectPropertyPage() {
     // TODO Auto-generated constructor stub
+    
   }
 
   @Override
   protected Control createContents(Composite parent) {
-    // TODO Auto-generated method stub
-    return null;
+    Composite composite = new Composite(parent, SWT.NONE);
+    
+    // TODO
+    
+    return composite;
+  }
+
+  public IProject getProject() {
+    IProject project = null;
+    IAdaptable element = getElement();
+    
+    if (element instanceof IProject) {
+      project = (IProject) element;
+    } else if (element != null) {
+      project = element.getAdapter(IProject.class);
+    }
+    return project;
   }
 
 }
