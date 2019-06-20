2bfa6b09529548e6819c0c21e58acf15f5f76968
Theo Hoekstra <theo.hoekstra@noreply.nl>
2019-06-20 18:19:29 +0200

    work on builder

--- src/nl/nnworks/nnembedded/plugin/project/builder/ProjectConfigBuilder.java
+++ src/nl/nnworks/nnembedded/plugin/project/builder/ProjectConfigBuilder.java
@@ -3,6 +3,7 @@ package nl.nnworks.nnembedded.plugin.project.builder;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -10,23 +11,48 @@ import org.eclipse.core.runtime.IProgressMonitor;
 public class ProjectConfigBuilder extends IncrementalProjectBuilder {
 
   public static final String BUILDER_ID = "nl.nnworks.nnembedded.plugin.projectconfigbuilder";
-  
+
   @Override
   protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor) throws CoreException {
     IProject project = getProject();
 
+    System.out.println("ProjectConfigBuilder build triggered for " + project.getName());
+    
     switch (kind) {
       case FULL_BUILD:
-        System.out.println("ProjectConfigBuilder full build triggered for " + project.getName());
+        fullBuild(monitor);
         break;
       case INCREMENTAL_BUILD:
-        System.out.println("ProjectConfigBuilder incremental build triggered for " + project.getName());
+        incrementalBuild(monitor);
         break;
       case AUTO_BUILD:
-        System.out.println("ProjectConfigBuilder auto build triggered for " + project.getName());
+        incrementalBuild(monitor);
         break;
     }
     return null;
+  }
+
+  protected void fullBuild(final IProgressMonitor monitor) {
+    System.out.println("performing full build");
+  }
+
+  protected void incrementalBuild(final IProgressMonitor monitor) {
+    final IProject project = getProject();
+    IResourceDelta delta = getDelta(project);
+    if (delta == null) {
+      fullBuild(monitor);
+    } else {
+      // do an incremental build
+      System.out.println("performing incremental build");
+    }
+  }
+
+  protected void startupOnInitialize() {
+    // add builder init logic here
+  }
+
+  protected void clean(IProgressMonitor monitor) {
+    // add builder clean logic here
   }
 
 }
